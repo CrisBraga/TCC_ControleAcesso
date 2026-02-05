34b0b198f97cdbf4d7ee26ae4c52b7f68e6a2a97
Cristhian <cris0412@gmail.com>
2026-02-05 15:30:55 -0300

    dando sequencia no projeto

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,17 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using QuestPDF.Infrastructure;
 
 namespace wpf_exemplo
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            QuestPDF.Settings.License = LicenseType.Community;
+        }
+    }
 }
